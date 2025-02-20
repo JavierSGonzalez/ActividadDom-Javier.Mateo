58a6c801a9814420df15f158acbe09af041a6a38
unknown <you@example.com>
2025-02-20 16:16:17 -0600

    Information

--- script.js
+++ script.js
@@ -10,6 +10,22 @@ const ht = document.createElement("p");
 ht.innerHTML = "HTML";
 document.body.appendChild(ht);
 
+const read = document.createElement("button");
+read.innerHTML = "Saber más";
+read.addEventListener("click", reemplazar);
+document.body.appendChild(read);
+
+
 function reemplazar(){
     document.getElementById("contenido").innerHTML = "HTML es un lenguaje usado para crear y estructurar páginas web. Utiliza etiquetas para definir cosas como títulos, párrafos, imágenes y enlaces dentro de una página. Es la base para mostrar contenido en Internet." 
+}
+
+const hide = document.createElement("button");
+hide.innerHTML = "Saber menos";
+hide.addEventListener("click", eliminar);
+document.body.appendChild(hide);
+
+
+function eliminar(){
+    document.getElementById("contenido").innerHTML = "" 
 }
