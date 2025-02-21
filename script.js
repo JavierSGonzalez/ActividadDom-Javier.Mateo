24ecb084f469c24f88042587820f5bf50b2bed36
unknown <you@example.com>
2025-02-20 19:15:00 -0600

    CSS explanation

--- script.js
+++ script.js
@@ -6,18 +6,23 @@ const des = document.createElement("h2");
 des.innerHTML = "Conceptos básicos de programación";
 document.body.appendChild(des);
 
-const ht = document.createElement("p");
-ht.innerHTML = "HTML";
-document.body.appendChild(ht);
-
 const read = document.createElement("button");
-read.innerHTML = "Saber más";
-read.addEventListener("click", reemplazar);
+read.innerHTML = "HTML";
+read.addEventListener("click", ht);
 document.body.appendChild(read);
 
-
-function reemplazar(){
+function ht(){
     document.getElementById("contenido").innerHTML = "HTML es un lenguaje usado para crear y estructurar páginas web. Utiliza etiquetas para definir cosas como títulos, párrafos, imágenes y enlaces dentro de una página. Es la base para mostrar contenido en Internet." 
+}
+
+const cs = document.createElement("button");
+cs.innerHTML = "CSS";
+cs.addEventListener("click", deco);
+document.body.appendChild(cs);
+
+
+function deco(){
+    document.getElementById("contenido").innerHTML = "CSS (Cascading Style Sheets) es un lenguaje de estilos utilizado para definir la apariencia y el diseño de páginas web. Permite controlar aspectos como colores, fuentes, márgenes, posiciones y animaciones, separando la estructura del contenido (HTML) del diseño visual." 
 }
 
 const hide = document.createElement("button");
@@ -29,3 +34,5 @@ document.body.appendChild(hide);
 function eliminar(){
     document.getElementById("contenido").innerHTML = "" 
 }
+
+document.getElementById("contenido").style.backgroundColor = "green";
